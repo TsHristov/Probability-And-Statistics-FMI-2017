bab1e65828bbf6c6de486a407ef0a40788b14fae
TsHristov <tsvetanhristov@yahoo.com>
2017-10-13 09:13:45 +0300

    Add coin toss task solution.

--- Week1/practice.R
+++ Week1/practice.R
@@ -19,7 +19,34 @@ minElement <- min(mat)
 mat <- rbind(mat, c(runif(n=2, min=1, max=20)))
 mat[,2] <- (mat[,2] * 2) + 6
 
-                  
+## Create a matrix with the probabilties of getting 'tails'
+## with n=10 coin tosses. The first column represents the
+## frequency vector μ/n, where μ=0:10 is the number of
+## getting 'tails'. In the second column are the probabilities
+## (C n μ /2 n), calculated with the function choose().
+## Plot the graphic using plot().
+
+require(graphics)
+
+tails.count <- c(0:10)
+
+probabilites <- c()
+
+i <- 0
+
+while(i < 10) {
+    res <- choose(10, i)
+    probabilites <- c(probabilites, res)
+    i <- i + 1
+}
+
+result = matrix(
+    c(tails.count, probabilites),
+    nrow = 10,
+    ncol = 2)
+
+plot(result)
+
 
 
 
