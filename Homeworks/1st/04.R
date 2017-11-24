e688399c250931ae66343222389788b55fcaad0b
TsHristov <tsvetanhristov@yahoo.com>
2017-11-24 10:30:31 +0200

    Add explanatory comments and histogram for the collected data

--- Homeworks/1st/04.R
+++ Homeworks/1st/04.R
@@ -1,12 +1,15 @@
+## Sample batteries characteristics:
+battery.lifetime.mean = 21
+battery.lifetime.variance = 16
 
 ## Production etalon battery characteristics:
 etalon.lifetime.mean = 20
 etalon.lifetime.variance = 4
 
 experiment = function(){
-    ## 5 batteries, with mean lifetime 21 and
+    ## 5 batteries with normal distribution, with mean lifetime 21 and
     ## lifetime variance 16:
-    return (rnorm(5, 21, 16))
+    return (rnorm(5, battery.lifetime.mean, battery.lifetime.variance))
 }
 
 repeat.experiment = function(n.times){
@@ -14,4 +17,8 @@ repeat.experiment = function(n.times){
     return (replicate(n.times, experiment()))
 }
 
-total = repeat.experiment(10)
+## Repeat the sampling experiment 1000 times:
+collected.data = repeat.experiment(1000)
+
+## Plot the result of the 1000 sampling experiments:
+hist(collected.data)
