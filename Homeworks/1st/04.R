53122bcfbb4aa24cb73d4e5cb60427807fb9fb74
TsHristov <tsvetanhristov@yahoo.com>
2017-11-25 19:30:22 +0200

    Add task 4 solution with explantory comments and visual comparison graphic

--- Homeworks/1st/04.R
+++ Homeworks/1st/04.R
@@ -1,24 +1,58 @@
-## Sample batteries characteristics:
-battery.lifetime.mean = 21
+library("ggplot2")
+
+## Sampled batteries characteristics:
+battery.lifetime.mean     = 21
 battery.lifetime.variance = 16
+battery.lifetime.sd       = sqrt(battery.lifetime.variance)
 
 ## Production etalon battery characteristics:
-etalon.lifetime.mean = 20
+etalon.lifetime.mean     = 20
 etalon.lifetime.variance = 4
+etalon.lifetime.sd       = sqrt(etalon.lifetime.variance)
 
-experiment = function(){
-    ## 5 batteries with normal distribution, with mean lifetime 21 and
-    ## lifetime variance 16:
-    return (rnorm(5, battery.lifetime.mean, battery.lifetime.variance))
+## Chi-Square significance level treshold:
+significance.level = .05 ## 95% of the batteries must be according to the etalon
+
+## Generate the batteries samples and etlons with their Normal Distribution:
+##     -samples - N(21,16)
+##     -etalons - N(20,4)
+generate.samples.and.etalons = function(batteries.count) {
+    observed.samples = rnorm(batteries.count, battery.lifetime.mean, battery.lifetime.sd)
+    expected.etalons = rnorm(batteries.count, etalon.lifetime.mean, etalon.lifetime.sd)
+    return (list("samples"=observed.samples, "etalons"=expected.etalons))
 }
 
-repeat.experiment = function(n.times){
-    ## Repeat the experiment n times:
-    return (replicate(n.times, experiment()))
+## Combine the battery samples and etalons into one dataframe:
+combine = function(observed.samples, expected.etalons){
+    observed.samples       = data.frame(x = observed.samples)
+    expected.etalons       = data.frame(x = expected.etalons)
+    observed.samples$name  = 'Sample'
+    expected.etalons$name  = 'Etalon'
+    combined               = rbind(observed.samples, expected.etalons)
+    return (combined)
 }
 
-## Repeat the sampling experiment 1000 times:
-collected.data = repeat.experiment(1000)
+## Use Chi-Square Goodness of Fit Test to compare the battery samples against the etalon:
+goodness.of.fit = function(observed.samples, expected.etalons){
+    return (chisq.test(observed.samples, p=expected.etalons, rescale.p=TRUE))
+}
 
-## Plot the result of the 1000 sampling experiments:
-hist(collected.data)
+all.samples      = generate.samples.and.etalons(10000)
+observed.samples = all.samples$samples
+expected.etalons = all.samples$etalons
+samples.etalons.combined = combine(observed.samples, expected.etalons)
+
+## Visual comparison of the battery samples and the expected etalon:
+png(filename="samples_to_etalons.png")
+print(ggplot(samples.etalons.combined, aes(x,fill=name)) + geom_histogram(alpha=0.5, position='identity',binwidth=1)+
+    ggtitle("Battery Samples vs Battery Etalons") + labs(x="Battery Lifetime", y="Count", fill="Battery Category:"))
+dev.off()
+
+## Perform Chi-Square Goodness of Fit Test:
+p.value = goodness.of.fit(observed.samples, expected.etalons)$p.value
+
+if (p.value < significance.level) {
+    print("Batteries are not according to etalon!")
+} else {
+    print("Batteries are according to etalon!")
+}
--- Homeworks/1st/README.md
+++ Homeworks/1st/README.md
@@ -1,4 +1,6 @@
 # Task 3:
   * `methods_comparison.png` - Comparison between used methods for dice throwing simulation.
+  * `samples_vs_etalons.png` - Comparison of battery samples against production etalon.
 
 ![alt tag](https://github.com/TsHristov/Probability-And-Statistics-FMI-2017/blob/master/Homeworks/1st/Plots/methods_comparison.png)
+![alt tag](https://github.com/TsHristov/Probability-And-Statistics-FMI-2017/blob/master/Homeworks/1st/Plots/samples_vs_etalons.png)
