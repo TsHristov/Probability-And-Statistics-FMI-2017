67addf3f472a1fc48cf97eeea6a621df50c95a41
TsHristov <tsvetanhristov@yahoo.com>
2018-02-07 11:01:07 +0200

    Add Iris data correlation plot and analysis

---
+++ Week9/README.md
@@ -0,0 +1,4 @@
+# Task 1:
+  * `iris_correlations.png` - plots the iris data correlation
+
+![alt tag](https://github.com/TsHristov/Probability-And-Statistics-FMI-2017/blob/master/Week9/Plots/iris_correlations.png)
---
+++ Week9/task1.R
@@ -0,0 +1,25 @@
+## Task 1:
+
+## Compute the iris data correlation:
+data(iris)
+
+## Remove categorical column ("Species"):
+iris = iris[,c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")]
+
+## Compute the correlation between Sepal.Length, Sepal.Width, Petal.Length, Petal.Width:
+print(cor(iris))
+
+## Observations: We see that Petal.Width and Petal.Length has a correlation coefficient of
+##               0.9628654, so we can conclude that they have a stron positive relationship:
+
+##              Sepal.Length Sepal.Width Petal.Length Petal.Width
+## Sepal.Length    1.0000000  -0.1175698    0.8717538   0.8179411
+## Sepal.Width    -0.1175698   1.0000000   -0.4284401  -0.3661259
+## Petal.Length    0.8717538  -0.4284401    1.0000000   0.9628654
+## Petal.Width     0.8179411  -0.3661259    0.9628654   1.0000000
+
+
+## Create visual representation of the correlations:
+## Once again, this time visually we observe the stron positive relationship between
+## Petal.Width and Petal.Length:
+pairs(iris)
